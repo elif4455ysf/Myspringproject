ef85bf0617d17817b5ab8fddef533231b4e90f14
elif4455ysf <150171216+elif4455ysf@users.noreply.github.com>
2025-01-30 19:24:03 +0200

    Add files via upload

---
+++ src/main/java/com/example/elifproject1/Elifproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.elifproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Elifproject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Elifproject1Application.class, args);
+    }
+
+
---
+++ src/test/java/com/example/elifproject1/Elifproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elifproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Elifproject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
